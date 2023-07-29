b24067895683538cf4420b61aa076103af2df6ba
Lewi97 <privkoen00@gmail.com>
2023-07-29 19:59:04 +0200

    print globals a bit nicer

--- LEngine/ByteCode.cpp
+++ LEngine/ByteCode.cpp
@@ -9,20 +9,20 @@ namespace le
 		string += "\nGlobals:\n";
 		for (auto count{ 0ull }; const auto & global : code.globals)
 		{
-			string += std::format("Index: {}\t", count++);
+			string += std::format("{}: ", count++);
 			switch (global->type)
 			{
 			case RuntimeValue::Type::Function:
 			{
 				const auto& function = static_cast<const CompiledFunction*>(global.get())->function_frame;
-				string += std::format("(Function '{}')\n{}\n", function.name, to_string(function, code));
+				string += std::format("\t(Function '{}')\n{}\n", function.name, to_string(function, code));
 				break;
 			}
 			case RuntimeValue::Type::String:
-				string += std::format("('{}')\n", global->make_string());
+				string += std::format("String('{}')\n", global->make_string());
 				break;
 			default:
-				string += "\nUnknown global type\n";
+				string += "Unknown global type";
 			}
 		}
 		return string;
