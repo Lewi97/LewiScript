6d34482c2111db0abadf2b2f4fab2de25fe9e6a8
Lewi97 <privkoen00@gmail.com>
2023-07-30 19:51:34 +0200

    made it a dll import

--- LibIO/LibIO.h
+++ LibIO/LibIO.h
@@ -2,8 +2,17 @@
 
 #include "../LEngine/Builtin.h"
 
+#ifdef LIB_EXPORT
+#define LE_LIBIO_API __declspec(dllexport)
+#else
+#define LE_LIBIO_API __declspec(dllimport)
+#endif
+
 namespace le::lib::io
 {
-	auto print(std::span<LeObject> args, struct MemoryManager&) -> LeObject;
+	extern "C" {
+		auto LE_LIBIO_API print(std::span<LeObject> args, struct MemoryManager&) -> LeObject;
+	}
 }
 
+#undef LE_LIBIO_API
