c65076ca172d8f241a2480298f39945d085a0a84
Lewi97 <privkoen00@gmail.com>
2023-07-30 17:08:38 +0200

    deprecated for now

--- LEngine/le_io.h
+++ LEngine/le_io.h
@@ -1,22 +1,26 @@
 #pragma once
 
-#include <span>
+/*
+* Currently deprecated
+*/
 
-#include "GlobalState.h"
-#include "builtin.h"
-
-namespace le::lib::io
-{
-	auto print(std::span<LeObject> args) -> LeObject
-	{
-		for (const auto& obj : args)
-		{
-			std::cout << obj->make_string() << ' ';
-		}
-
-		std::cout << '\n';
-
-		return global::mem->emplace<NullValue>();
-	}
-}
+//#include <span>
+//
+//#include "GlobalState.h"
+//#include "builtin.h"
+//
+//namespace le::lib::io
+//{
+//	auto print(std::span<LeObject> args) -> LeObject
+//	{
+//		for (const auto& obj : args)
+//		{
+//			std::cout << obj->make_string() << ' ';
+//		}
+//
+//		std::cout << '\n';
+//
+//		return global::mem->emplace<NullValue>();
+//	}
+//}
 
