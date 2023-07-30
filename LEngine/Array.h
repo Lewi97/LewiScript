eb95c24e78f01c02499b0faa6b9239de3d3c8cd8
Lewi97 <privkoen00@gmail.com>
2023-07-30 23:08:01 +0200

    append and size member functions

--- LEngine/Array.h
+++ LEngine/Array.h
@@ -41,17 +41,33 @@ namespace le
 
 		auto access(LeObject index) -> LeObject override
 		{
-			//member_functions
-			if (index->type == Type::String)
-			{
-				auto str = static_cast<StringValue*>(index.get());
-				if (str->string == "append")
-				{
-
-				}
-			}
 			auto idx = to_numeric_index(*index);
 			return at(idx);
+		}
+
+		auto member_access(LeObject self, LeObject query) -> LeObject
+		{
+			auto& str = static_cast<StringValue*>(query.get())->string;
+			if (str == "append")
+			{
+				return global::mem->emplace<MemberFunction<Array>>(self,
+					[](Array& self, std::span<LeObject>& args, struct VirtualMachine&)->LeObject
+					{
+						self.data.append_range(args);
+						return self.data.back();
+					}
+				);
+			}
+			if (str == "size")
+			{
+				return global::mem->emplace<MemberFunction<Array>>(self,
+					[](Array& self, std::span<LeObject>& args, struct VirtualMachine&)->LeObject
+					{
+						return global::mem->emplace<NumberValue>(self.data.size());
+					}
+				);
+			}
+			throw(ferr::invalid_member(str));
 		}
 
 		auto at(size_t idx) -> LeObject&
--- LEngine/MemberFunctions.h
+++ LEngine/MemberFunctions.h
@@ -1,60 +1,28 @@
 #pragma once
 
 #include <concepts>
-#include <vector>
 
 #include "Builtin.h"
 
 namespace le
 {
-    //template<typename _This, size_t _size>
-    //    //requires(std::derived_from<_This, RuntimeValue>)
-    //struct MemberFunctionStorage
-    //{
-    //    using Id = unsigned;
-    //    using Args = std::vector<LeObject>;
-    //    using MemberFunction = LeObject(*)(_This&, Args& args);
-    //    using Container = std::array<MemberFunction, _size>;
-
-    //    MemberFunctionStorage() = default;
-
-    //    auto add(Id id, MemberFunction func) -> void
-    //    {
-    //        if (id > _size)
-    //            ferr::index_out_of_range(id);
-    //        functions[id] = func;
-    //    }
-
-    //    auto call(_This& this_, Id id, Args& args) -> LeObject
-    //    {
-    //        if (id > _size)
-    //            ferr::index_out_of_range(id);
-    //        if (functions.at(id) == nullptr)
-    //            ferr::make_exception("Tried to call non existing member function");
-
-    //        return functions.at(id)(*this_, args);
-    //    }
-    //private:
-    //    Container functions{};
-    //};
-
-    /*(TODO) Some way to either allow member functions to not be stored in variables or keep alive the parent */
     template<typename _This>
     struct MemberFunction
         : RuntimeValue
     {
-        using Function = LeObject(*)(_This&, std::vector<LeObject>& args);
-        MemberFunction(_This& object, Function func)
+        using Function = LeObject(*)(_This&, std::span<LeObject>&, struct VirtualMachine&);
+
+        MemberFunction(LeObject object, Function func)
             : _this(object)
             , _function(func)
         {}
 
-        auto call(std::vector<LeObject>& args) -> LeObject override
+        auto call(std::span<LeObject>& args, struct VirtualMachine& vm) -> LeObject override
         {
-            return _function(_this, args);
+            return _function(*static_cast<_This*>(_this.get()), args, vm);
         }
     private:
-        _This& _this{};
+        LeObject _this{};
         Function _function{};
     };
 }
