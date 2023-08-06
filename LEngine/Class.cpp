8ffd34b055e8016202855e4410755662bc153831
Lewi97 <privkoen00@gmail.com>
2023-08-06 17:55:31 +0200

    added make member function

---
+++ LEngine/Class.cpp
@@ -0,0 +1,7 @@
+#include "Class.h"
+#include "VM.h"
+
+//auto le::ClassMemberFunction::call(std::span<LeObject>& args, VirtualMachine& vm) -> LeObject
+//{ /* Add this ptr as well */
+//	return vm.run(function_frame, args, this_ptr);
+//}
