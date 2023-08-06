5433b0c90a9bb339d0a938b7fe3c369c81acfc58
Lewi97 <privkoen00@gmail.com>
2023-08-06 17:57:00 +0200

    added builtinmemberfunction object

--- LEngine/MemberFunctions.cpp
+++ LEngine/MemberFunctions.cpp
@@ -1,1 +1,13 @@
 #include "MemberFunctions.h"
+#include "VM.h"
+#include "ByteCode.h"
+
+auto le::BuiltinMemberFunction::call(std::span<LeObject>& args, VirtualMachine& vm) -> LeObject
+{
+	return vm.run(_function, args, _this);
+}
+
+auto le::BuiltinMemberFunction::type_name() -> String
+{
+	return std::format("{}::{}", _this->type_name(), _function.name);
+}
--- LEngine/MemberFunctions.h
+++ LEngine/MemberFunctions.h
@@ -30,5 +30,21 @@ namespace le
         LeObject _this{};
         Function _function{};
     };
+
+    struct BuiltinMemberFunction
+        : RuntimeValue
+    {
+        BuiltinMemberFunction(LeObject self, const struct Frame& func)
+            : _this(self)
+            , _function(func)
+        {}
+
+        auto call(std::span<LeObject>& args, class VirtualMachine& vm) -> LeObject override;
+
+        auto type_name() -> String override;
+    private:
+        LeObject _this{};
+        const struct Frame& _function;
+    };
 }
 
